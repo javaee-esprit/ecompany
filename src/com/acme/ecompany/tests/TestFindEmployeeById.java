e0a02681c9fe77609b0cf920ca25275fed3ef51e
KL <karim.lahouague@esprit.tn>
2014-10-11 09:56:54 +0100

    initial commit 2

--- src/com/acme/ecompany/tests/TestFindEmployeeById.java
+++ src/com/acme/ecompany/tests/TestFindEmployeeById.java
@@ -4,13 +4,14 @@
  */
 package com.acme.ecompany.tests;
 
+import com.acme.ecompany.persistence.model.Employee;
 import java.sql.*;
 
 /**
  *
  * @author Karim
  */
-public class TestCreateEmployee {
+public class TestFindEmployeeById {
     
     public static void main(String[] args){
         
@@ -19,12 +20,24 @@ public class TestCreateEmployee {
         String password = "root";
         Connection connection = null;
         Statement stmt = null;
-        String sql = "INSERT INTO T_EMPLOYEE (ID,NAME,SALARY) VALUES (2, 'Kenza', 5001)";
+        ResultSet rs = null;
+        Employee found = null;
+        String sql = "SELECT * FROM T_EMPLOYEE WHERE ID='2'";
         try{
             connection = DriverManager.getConnection(url, user, password);
             System.out.println("CONNECTION ESTABLISHED");
             stmt = connection.createStatement();
-            stmt.executeUpdate(sql);
+            rs = stmt.executeQuery(sql);
+            if(rs.next()){
+                int employeeId = rs.getInt("ID");
+                String employeeName = rs.getString("NAME");
+                double employeeSalary = rs.getDouble("SALARY");
+                found = new Employee(employeeId,employeeName,employeeSalary);
+                
+                
+            }
+            System.out.println(found);
+                
         }catch(SQLException  ex){
             System.out.println("PROBLEM OCCURED : " + ex.getMessage());
         }
